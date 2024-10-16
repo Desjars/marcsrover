96f9a6c40e774cee93045812f957fd90bf355731
Enzo Le Van <dev@enzo-le-van.fr>
2024-10-16 11:06:58 +0200

    draw LiDAR

--- nodes/monitoring.py
+++ nodes/monitoring.py
@@ -37,8 +37,11 @@ class Monitoring:
             dpg.add_image("realsense_color", pos=(0, 0))
             dpg.add_image("realsense_depth", pos=(640, 0))
 
-        with dpg.window(tag="LiDAR", width=640, height=480, pos=(0, 480)):
-            pass
+        with dpg.window(label="LiDAR", tag="LiDAR", width=640, height=480, pos=(0, 480)):
+            dpg.add_slider_float(label="LiDAR Scale", tag="LiDAR Scale", width=150, min_value=1, max_value=200, default_value=100)
+
+            with dpg.drawlist(width=640, height=480) as self.lidar_canvas:
+                pass
 
         # Create zenoh session
         config = zenoh.Config.from_file("zenoh_config.json")
@@ -77,7 +80,38 @@ class Monitoring:
         dpg.destroy_context()
 
     def lidar_callback(self, sample):
+        lidar_scale = dpg.get_value("LiDAR Scale")
+
         lidar = LidarScan.deserialize(sample.value.payload)
+
+        dpg.delete_item(self.lidar_canvas)
+        with dpg.drawlist(width=640, height=480, parent="LiDAR") as self.lidar_canvas:
+            radius = 190
+
+            dpg.draw_circle(center=(320, 230), radius=radius, color=(255, 255, 255, 255), thickness=1)
+            dpg.draw_circle(center=(320, 230), radius=5, color=(255, 0, 0, 255), thickness=5)
+
+            for i in range(0, 360, 30):
+                x = 320 + radius * np.cos(np.radians(i))
+                y = 230 + radius * np.sin(np.radians(i))
+                dpg.draw_line((320, 230), (x, y), color=(255, 255, 255, 255), thickness=1)
+
+                text_x = 320 + (radius + 20) * np.cos(np.radians(i)) - i * 20/360
+                text_y = 230 + (radius + 20) * np.sin(np.radians(i)) - i * 20/360
+
+                dpg.draw_text((text_x, text_y), str(i), color=(255, 255, 255, 255), size=16)
+
+            for i in range (len(lidar.angles)):
+                angle = lidar.angles[i]
+                distance = lidar.distances[i]
+
+                x = 320 + distance * np.cos(np.radians(angle)) * lidar_scale / 200
+                y = 230 + distance * np.sin(np.radians(angle)) * lidar_scale / 200
+
+                if np.sqrt((x - 320) ** 2 + (y - 230) ** 2) > radius:
+                    continue
+
+                dpg.draw_circle((int(x), int(y)), 1, color=(0, 0, 255, 255), thickness=5)
 
     def realsense_callback(self, sample):
         image = D435I.deserialize(sample.value.payload)
