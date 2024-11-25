0e9f207b33fa62015a89621ac82a5e35d2dcc214
Enzo Le Van <dev@enzo-le-van.fr>
2024-11-25 10:49:04 +0100

    separate car and host code

--- src/marcsrover/__init__.py
+++ src/marcsrover/__init__.py
@@ -1,16 +1,17 @@
-from marcsrover.host import run as run_host
-from marcsrover.car import run as run_car
-
 import sys
 
 
 def main_host() -> None:
+    from marcsrover.host import run as run_host
+
     address_to_connect_to = sys.argv[1] if len(sys.argv) > 1 else None
 
     run_host(address_to_connect_to)
 
 
 def main_car() -> None:
+    from marcsrover.car import run as run_car
+
     address_to_listen_on = sys.argv[1] if len(sys.argv) > 1 else None
 
     run_car(address_to_listen_on)
