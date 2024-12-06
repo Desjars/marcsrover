ec25ba88b0c0950e3e5f6ebb0558ae2f7949bfe8
Enzo Le Van <dev@enzo-le-van.fr>
2024-12-06 10:31:28 +0100

    fix devices

--- src/marcsrover/arguments.py
+++ src/marcsrover/arguments.py
@@ -14,21 +14,21 @@ def main_args():
     parser.add_argument(
         "--servo-port",
         type=str,
-        default="/dev/ttyACM1",
+        default="/dev/tty_SERVO",
         help="Serial port for the connection to the servo motor.",
     )
 
     parser.add_argument(
         "--microcontroller-port",
         type=str,
-        default="/dev/ttyACM0",
+        default="/dev/tty_STM32",
         help="Serial port for the connection to the microcontroller.",
     )
 
     parser.add_argument(
         "--lidar-port",
         type=str,
-        default="/dev/ttyUSB0",
+        default="/dev/tty_LIDAR",
         help="Serial port for the connection to the LiDAR.",
     )
 
